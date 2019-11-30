8427370dd099011404755fd5db63e68e255a83ab
gyucode <gyucode@gmail.com>
2019-11-30 14:33:35 +0900

    LinearLayout / TextView 속성연습

--- app/src/main/java/kr/co/tjoeun/a20181130_01_firstapp/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20181130_01_firstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.a20181130_01_firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //주석
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
